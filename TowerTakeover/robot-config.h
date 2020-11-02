082b93916383cf608608f7458e54800b25938112
Tarang Lunawat <tangluna@users.noreply.github.com>
2020-11-01 20:51:01 -0800

    Create robot-config.h

---
+++ TowerTakeover/robot-config.h
@@ -0,0 +1,18 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern controller Controller1;
+extern motor ARM_LEFT;
+extern motor ARM_RIGHT;
+extern motor INTAKE_LEFT;
+extern motor INTAKE_RIGHT;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
